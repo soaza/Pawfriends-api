396deea793b43eb42299432ca5e7dfa8ce99b193
soaza <65228562+soaza@users.noreply.github.com>
2021-06-03 20:25:50 +0800

    Data

--- model/data.sql
+++ model/data.sql
@@ -8,9 +8,11 @@ INSERT INTO database_dogs VALUES (6,'Spotty','Male',null);
 INSERT INTO database_cms_account VALUES ('pawfriends_admin','maple123');
 INSERT INTO database_cms_account VALUES ('pawfriends_exco','ilovelucky123');
 
-INSERT INTO database_exco VALUES(1,'Exco 1',1,'Hobby 1','Lucky');
-INSERT INTO database_exco VALUES(2,'Exco 1',1,'Hobby 1','Lucky');
-INSERT INTO database_exco VALUES(3,'Exco 1',1,'Hobby 1','Lucky');
-INSERT INTO database_exco VALUES(4,'Exco 1',1,'Hobby 1','Lucky');
+INSERT INTO database_exco VALUES (1,'Exco 1',1,'Hobby 1','Lucky');
+INSERT INTO database_exco VALUES (2,'Exco 1',1,'Hobby 1','Lucky');
+INSERT INTO database_exco VALUES (3,'Exco 1',1,'Hobby 1','Lucky');
+INSERT INTO database_exco VALUES (4,'Exco 1',1,'Hobby 1','Lucky');
 
 INSERT INTO database_main_page VALUES('Hi,Welcome to Pawfriends!');
+
+INSERT INTO database_activity_posts VALUES (null,'We walked Hugo today!');
